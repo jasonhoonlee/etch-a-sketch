7e445e8fb794195aa6ce68ea416e75c7f2132ffc
Hoon Lee <jasonhoonlee@gmail.com>
2022-06-04 16:14:37 -0700

    Refactor 'createGridRows' and 'createGridCells' function
    -purpose: Make both functions flexible, should take an input of gridSize, as opposed to having a fixed row and cell count.
    
    The function will allow grid size to adjust dynamically, as user changes input.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const gridCanvas = document.querySelector('.grid-canvas');
 
 //create rows
-function createGridRows() {
-  for (let i = 1; i < 11; i++) {
+function createGridRows(gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     row.classList.add(`grid-row-${i}`);
-    createGridCells(row);
+    createGridCells(row, gridSize);
     gridCanvas.appendChild(row);
   }
 }
 
 //create cells
-function createGridCells(row) {
-  for (let i = 1; i < 11; i++) {
+function createGridCells(row, gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     row.appendChild(cell);
   }
 }
 
-createGridRows();
+createGridRows(64);
 
 
