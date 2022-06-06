eced1668cd61535cae6dd46c518ce43aad2e6f7c
Hoon Lee <jasonhoonlee@gmail.com>
2022-06-06 14:28:44 -0700

    Change 'updateGrid' function name tp 'updateGridSize'

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const slider = document.querySelector('.grid-range');
 const gridBackgroundColor = document.querySelector('.grid-background-color .color-picker');
 const gridLineColor = document.querySelector('.grid-color .color-picker');
 
+
 //create rows
 function createGridRows(gridSize) {
   for (let i = 1; i <= gridSize; i++) {
@@ -28,7 +29,7 @@ function updateGridSizeInfo(size) {
   currentGridSize.textContent = `${size} x ${size}`;
 }
 
-function updateGrid(size) {
+function updateGridSize(size) {
   gridCanvas.innerHTML = '';
   createGridRows(size);
 }
@@ -50,7 +51,7 @@ function updateGridLineColor(color) {
 slider.addEventListener('input', e => {
   if (e.target.tagName !== 'INPUT') return;
   const gridSize = e.target.value;
-  updateGrid(gridSize);
+  updateGridSize(gridSize);
   updateGridSizeInfo(gridSize);
 })
 
@@ -63,6 +64,8 @@ gridLineColor.addEventListener('input', e => {
   const gridColor = e.target.value;
   updateGridLineColor(gridColor);
 })
+
+
 
 createGridRows(32);
 
